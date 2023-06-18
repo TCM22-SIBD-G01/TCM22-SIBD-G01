8e7807749c3b502ac5f1d32d49e49351de7a2440
FariaAna <anafaria0254@gmail.com>
2023-06-18 17:06:48 +0100

    Update data (sql+csv)

---
+++ database/tabledata/sql/Efetua.sql
@@ -0,0 +1,36 @@
+/*
+-- Query: SELECT * FROM andante_database.Efetua
+LIMIT 0, 1000
+
+-- Date: 2023-06-18 16:49
+*/
+INSERT INTO `` (`EfetuaID`,`CartaoID`,`ViagemID`) VALUES (1,125872,151);
+INSERT INTO `` (`EfetuaID`,`CartaoID`,`ViagemID`) VALUES (2,194512,189);
+INSERT INTO `` (`EfetuaID`,`CartaoID`,`ViagemID`) VALUES (3,213758,198);
+INSERT INTO `` (`EfetuaID`,`CartaoID`,`ViagemID`) VALUES (4,271888,203);
+INSERT INTO `` (`EfetuaID`,`CartaoID`,`ViagemID`) VALUES (5,282346,264);
+INSERT INTO `` (`EfetuaID`,`CartaoID`,`ViagemID`) VALUES (6,287936,298);
+INSERT INTO `` (`EfetuaID`,`CartaoID`,`ViagemID`) VALUES (7,346277,326);
+INSERT INTO `` (`EfetuaID`,`CartaoID`,`ViagemID`) VALUES (8,357198,327);
+INSERT INTO `` (`EfetuaID`,`CartaoID`,`ViagemID`) VALUES (9,369215,387);
+INSERT INTO `` (`EfetuaID`,`CartaoID`,`ViagemID`) VALUES (10,418264,419);
+INSERT INTO `` (`EfetuaID`,`CartaoID`,`ViagemID`) VALUES (11,421639,442);
+INSERT INTO `` (`EfetuaID`,`CartaoID`,`ViagemID`) VALUES (12,429700,445);
+INSERT INTO `` (`EfetuaID`,`CartaoID`,`ViagemID`) VALUES (13,503639,503);
+INSERT INTO `` (`EfetuaID`,`CartaoID`,`ViagemID`) VALUES (14,512446,517);
+INSERT INTO `` (`EfetuaID`,`CartaoID`,`ViagemID`) VALUES (15,535640,532);
+INSERT INTO `` (`EfetuaID`,`CartaoID`,`ViagemID`) VALUES (16,561322,535);
+INSERT INTO `` (`EfetuaID`,`CartaoID`,`ViagemID`) VALUES (17,569843,563);
+INSERT INTO `` (`EfetuaID`,`CartaoID`,`ViagemID`) VALUES (18,649723,586);
+INSERT INTO `` (`EfetuaID`,`CartaoID`,`ViagemID`) VALUES (19,684901,606);
+INSERT INTO `` (`EfetuaID`,`CartaoID`,`ViagemID`) VALUES (20,690422,634);
+INSERT INTO `` (`EfetuaID`,`CartaoID`,`ViagemID`) VALUES (21,727904,669);
+INSERT INTO `` (`EfetuaID`,`CartaoID`,`ViagemID`) VALUES (22,745960,687);
+INSERT INTO `` (`EfetuaID`,`CartaoID`,`ViagemID`) VALUES (23,758325,705);
+INSERT INTO `` (`EfetuaID`,`CartaoID`,`ViagemID`) VALUES (24,758478,717);
+INSERT INTO `` (`EfetuaID`,`CartaoID`,`ViagemID`) VALUES (25,793845,748);
+INSERT INTO `` (`EfetuaID`,`CartaoID`,`ViagemID`) VALUES (26,812139,762);
+INSERT INTO `` (`EfetuaID`,`CartaoID`,`ViagemID`) VALUES (27,831106,785);
+INSERT INTO `` (`EfetuaID`,`CartaoID`,`ViagemID`) VALUES (28,876553,812);
+INSERT INTO `` (`EfetuaID`,`CartaoID`,`ViagemID`) VALUES (29,887501,821);
+INSERT INTO `` (`EfetuaID`,`CartaoID`,`ViagemID`) VALUES (30,946136,828);
---
+++ database/tabledata/sql/File.sql
@@ -0,0 +1,57 @@
+CREATE TABLE `test`.`Utilizador` (
+  `NIF` INT(9) NOT NULL,
+  `UtilizadorID` INT(5) NOT NULL,
+  `Nome` VARCHAR(45) NOT NULL,
+  `Email` VARCHAR(45) NOT NULL,
+  `NumeroTelemovel` INT(9) NOT NULL,
+  `Endereco` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`NIF`),
+  UNIQUE INDEX `NIF_UNIQUE` (`NIF` ASC) VISIBLE,
+  UNIQUE INDEX `UtilizadorID_UNIQUE` (`UtilizadorID` ASC) VISIBLE);
+  
+  CREATE TABLE `test`.`Assinatura` (
+  `AssinaturaID` INT(11) NOT NULL,
+  `TipodeAssinatura` VARCHAR(45) NOT NULL,
+  `Validade` DATE NOT NULL,
+  `Preco` DECIMAL(10,2) NOT NULL,
+  PRIMARY KEY (`AssinaturaID`),
+  UNIQUE INDEX `AssinaturaID_UNIQUE` (`AssinaturaID` ASC) VISIBLE);
+  
+CREATE TABLE `test`.`CartaoAndante` (
+  `CartaoID` INT(11) NOT NULL,
+  `Validade` DATE NOT NULL,
+  PRIMARY KEY (`CartaoID`),
+  UNIQUE INDEX `CartaoID_UNIQUE` (`CartaoID` ASC) VISIBLE);
+  
+CREATE TABLE `test`.`Viagem` (
+  `ViagemID` INT(11) NOT NULL,
+  `InicioViagem` VARCHAR(45) NOT NULL,
+  `DataViagem` DATE NOT NULL,
+  `Validacao` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`ViagemID`),
+  UNIQUE INDEX `ViagemID_UNIQUE` (`ViagemID` ASC) VISIBLE);
+CREATE TABLE `test`.`Contem` (
+  `ContemID` INT(11) NOT NULL AUTO_INCREMENT,
+  `Contem_AssinaturaID` INT(11) NOT NULL,
+  `Contem_CartaoID` INT(11) NOT NULL,
+  PRIMARY KEY (`ContemID`),
+  UNIQUE INDEX `ContemID_UNIQUE` (`ContemID` ASC) VISIBLE);  
+  
+CREATE TABLE `Possui_um` (
+  `NIF` int(9) NOT NULL,
+  `CartaoID` int(11) NOT NULL,
+  KEY `NIF_idx` (`NIF`),
+  KEY `CartaoID_idx` (`CartaoID`),
+  CONSTRAINT `CartaoID` FOREIGN KEY (`CartaoID`) REFERENCES `CartaoAndante` (`CartaoID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `NIF` FOREIGN KEY (`NIF`) REFERENCES `Utilizador` (`NIF`) ON DELETE NO ACTION ON UPDATE CASCADE);
+
+CREATE TABLE `Efetua` (
+  `EfetuaID` int(11) NOT NULL AUTO_INCREMENT,
+  `CartaoID` int(11) NOT NULL,
+  `ViagemID` int(11) NOT NULL,
+  PRIMARY KEY (`EfetuaID`),
+  UNIQUE KEY `EfetuaID_UNIQUE` (`EfetuaID`),
+  KEY `Efetua_ViagemID` (`ViagemID`),
+  KEY `Efetua_CartaoID` (`CartaoID`),
+  CONSTRAINT `Efetua_CartaoID` FOREIGN KEY (`CartaoID`) REFERENCES `CartaoAndante` (`CartaoID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `Efetua_ViagemID` FOREIGN KEY (`ViagemID`) REFERENCES `Viagem` (`ViagemID`) ON DELETE NO ACTION ON UPDATE NO ACTION);
